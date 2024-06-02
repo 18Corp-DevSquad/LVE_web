d7e620a98563aed3fe149216ed3f7e9ae8daba69
BuildTools <unconfigured@null.spigotmc.org>
2024-06-02 23:32:08 +0200

    ScrollTopBtn

--- docs/nav.js
+++ docs/nav.js
@@ -3,3 +3,20 @@ function toggleMenu()
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+
+window.onscroll = function() {scroll()};
+
+function scroll(){
+    let topBtn = document.getElementById("topBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+      } else {
+        topBtn.style.display = "none";
+      }
+}
+
+
+function ScrollTopFunction() {
+    document.body.scrollTop = 0; // pour Safari
+    document.documentElement.scrollTop = 0; // pour Chrome, Firefox, Opera, etc.
+  }
