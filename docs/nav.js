27bead807acb232f815a826a6331759a5e8c82ce
H18 <gael17320@hotmail.com>
2024-06-03 18:08:45 +0200

    img top

--- docs/nav.js
+++ docs/nav.js
@@ -1,32 +1,45 @@
 function toggleMenu()
 {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
+	const navLinks = document.querySelector('.nav-links');
+	navLinks.classList.toggle('active');
 }
 
 window.onscroll = function() {scroll()};
 
-function scroll(){
+function scroll()
+{
     let topBtn = document.getElementById("topBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        topBtn.style.transform = "translateX(-200px)";
-      } else {
-        topBtn.style.transform = "translateX(0px)";
-      }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+	{
+    	topBtn.style.transform = "translateX(-200px)";
+    }
+	else
+	{
+    	topBtn.style.transform = "translateX(0px)";
+    }
     const footer = document.querySelector('footer');
     const footerRect = footer.getBoundingClientRect();
     const windowHeight = window.innerHeight;
-    if (footerRect.top < windowHeight) {
-      let distanceToBottom = windowHeight - footerRect.top;
-      topBtn.style.bottom = `${distanceToBottom}px`;
-    } else {
-        topBtn.style.bottom = '0px';
+    if (footerRect.top < windowHeight)
+	{
+    	let distanceToBottom = windowHeight - footerRect.top;
+    	topBtn.style.bottom = `${distanceToBottom}px`;
+    }
+	else
+	{
+    	topBtn.style.bottom = '0px';
     }
 }
 
 
-function ScrollTopFunction() {
-    document.body.scrollTop = 0; // pour Safari
-    document.documentElement.scrollTop = 0; // pour Chrome, Firefox, Opera, etc.
-  }
+function ScrollTopFunction()
+{
+	document.body.scrollTop = 0; // pour Safari
+	document.documentElement.scrollTop = 0; // pour Chrome, Firefox, Opera, etc.
+}
 
+window.addEventListener('scroll', function()
+{
+	let scrollPosition = document.documentElement.scrollTop;
+	parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+});
