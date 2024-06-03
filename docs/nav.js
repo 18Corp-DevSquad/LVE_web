733db705e8ee1bc66a4d291ac8b038c9bf2dd5cc
H18 <gael17320@hotmail.com>
2024-06-03 15:47:55 +0200

    topbtn style

--- docs/nav.js
+++ docs/nav.js
@@ -8,11 +8,20 @@ window.onscroll = function() {scroll()};
 
 function scroll(){
     let topBtn = document.getElementById("topBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topBtn.style.display = "block";
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        topBtn.style.transform = "translateX(-200px)";
       } else {
-        topBtn.style.display = "none";
+        topBtn.style.transform = "translateX(0px)";
       }
+    const footer = document.querySelector('footer');
+    const footerRect = footer.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    if (footerRect.top < windowHeight) {
+      let distanceToBottom = windowHeight - footerRect.top;
+      topBtn.style.bottom = `${distanceToBottom}px`;
+    } else {
+        topBtn.style.bottom = '0px';
+    }
 }
 
 
@@ -20,3 +29,4 @@ function ScrollTopFunction() {
     document.body.scrollTop = 0; // pour Safari
     document.documentElement.scrollTop = 0; // pour Chrome, Firefox, Opera, etc.
   }
+
