e8d055e43413c6716f9407e4d6795a08729ea6a6
BuildTools <unconfigured@null.spigotmc.org>
2024-06-04 23:03:22 +0200

    search bar

--- docs/nav.js
+++ docs/nav.js
@@ -43,3 +43,37 @@ window.addEventListener('scroll', function()
 	let scrollPosition = document.documentElement.scrollTop;
 	parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
 });
+
+
+const page_index_list = [
+	{ title: "Actualité", url: "/actualite.html", content:"Actualités du site et du serveur, retrouvez toutes les informations nécéssaire sur les actualités d'LVE"},
+	{ title: "Menu principal", url: "/", content: "Page principale du site"},
+	{ title: "Encyclopédie", url: "/encyclopedie.html", content: "Apprenez en plus sur l'univers d'LVE"}
+]
+
+function searchPages() {
+	const query = document.getElementById('search-input').value.toLowerCase();
+	const resultsContainer = document.getElementById('results');
+	resultsContainer.innerHTML = '';
+
+	if (query) {
+		const filteredPages = page_index_list.filter(page_index_list => 
+			page_index_list.title.toLowerCase().includes(query) || page_index_list.content.toLowerCase().includes(query)
+		);
+
+		if (filteredPages.length > 0) {
+			resultsContainer.style.display = 'block';
+			filteredPages.forEach(page_index_list => {
+				const resultItem = document.createElement('div');
+				resultItem.classList.add('result-item');
+				resultItem.innerHTML = `<a href="${page_index_list.url}">${page_index_list.title}</a>`;
+				resultsContainer.appendChild(resultItem);
+			});
+		} else {
+			resultsContainer.style.display = 'block';
+			resultsContainer.innerHTML = '<p>No results found</p>';
+		}
+	} else {
+		resultsContainer.style.display = 'none';
+	}
+}
